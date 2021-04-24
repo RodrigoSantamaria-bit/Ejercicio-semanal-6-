684438c94a89b3e44e70b0226f40b70c1ab3fd99
Rodrigo Santamaria <>
2021-04-24 16:57:12 +0000

    ejercicio 1

---
+++ Ejercicio1.js
@@ -0,0 +1,15 @@
+//Ejercicio 1 
+let num 
+// js utiliza console.log como medio de impresion :o
+// En este caso, scanf no es una funcion de js :(. Tendrias que utilizar prompt
+var num = prompt('Introduce el numero que quieras saber como se escribe:');
+// En este caso, por ser una comparacion, se debe de escribir == en vez de =
+//okay :D
+if (num == 1){
+  //En este caso, js no puede leer un string sin ser string :(, necesita ""
+  cosole.log("El numero que escribiste es uno");
+}else if(num == 2){
+  console.log("El numero que escribiste es dos"); 
+} 
+
+//oki ;D
